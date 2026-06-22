7cdb8ef4588e5fc1b5b2a2e3276d9461e59c4656
Harrizon <harryalex0281@hotmail.com>
2026-06-22 15:09:49 -0500

    feat: add student report service

--- src/student_report_service.py
+++ src/student_report_service.py
@@ -1,9 +1,35 @@
+import logging
 from dataclasses import dataclass
 
+from homologable_subjects_service import HomologableSubjectsService
 from student_info_gateway import StudentInfoGateway
 from student_report_gateway import StudentReportGateway
+from subject_repository import SubjectRepository
+from shared_types import ReportGenerationResult
+from entities import Student
 
 @dataclass(frozen=True)
 class StudentReportService:
     _info_gateway: StudentInfoGateway
     _report_gateway: StudentReportGateway
+    _subject_repository: SubjectRepository
+    _logger: logging.Logger
+
+    def generate_report(self) -> ReportGenerationResult:
+        student = self._build_student()
+
+        return self._report_gateway.generate_report(student)
+
+    def _build_student(self) -> Student:
+        homologable_subjects = HomologableSubjectsService(
+            self._subject_repository,
+            self._info_gateway,
+            self._logger
+        ).suggest_subjects()
+
+        return Student(
+            self._info_gateway.get_name(),
+            self._info_gateway.get_identification(),
+            homologable_subjects
+        )
+        
